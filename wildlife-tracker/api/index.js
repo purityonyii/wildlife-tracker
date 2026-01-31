a62b196531c0a102796b9d21707cb81f1aacaba5
Onyinyechi Rita Ngaokere <ngaokere.o.rita@gmail.com>
2026-01-30 23:06:52 -0500

    Fix Vercel API route

--- wildlife-tracker/api/index.js
+++ wildlife-tracker/api/index.js
@@ -1,16 +1,6 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, "..", "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-});
-
-module.exports = app;
+module.exports = (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "API is working ✅"
+  });
+};
